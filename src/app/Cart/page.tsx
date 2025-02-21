1df8bd4b61d4ed2a4eab0183fa41041c9119d450
iiiamken <kenny.t.dinh93@gmail.com>
2025-02-21 23:58:25 +0100

    changed name and added classes

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -1,3 +1,5 @@
-export default function Cart() {
-  return <div className=""></div>
+export default function Page() {
+  return (
+    <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 lg:max-w-7xl lg:px-8"></div>
+  )
 }
